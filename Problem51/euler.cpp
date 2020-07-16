16c36a2f20592090a96a036ec31b7c567d4965e3
dptheodore <dptheodore93@gmail.com>
2020-07-16 01:34:15 -0400

    added folders and files for upcoming problems

---
+++ Problem51/euler.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+	int specialNum = 100;
+
+	//test all combinations of num being prime fam
+	//move to next number in ones place
+	//repeat til got an eight banger
+
+
+	
+	cout << specialNum << endl;
+	return 0;
+}
