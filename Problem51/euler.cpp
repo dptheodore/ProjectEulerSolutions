2b7f4c68c14bcf33859f8fafa666c90c12b395de
dptheodore <dptheodore93@gmail.com>
2020-07-18 00:43:48 -0400

    work on problem 51

--- Problem51/euler.cpp
+++ Problem51/euler.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+int factorial(int n){
+	return (n <= 1) ? 1 : n * factorial(n-1);
+}
+int getCombos(int numDigits){
+	int top = numDigits-1;
+	int combos = 0;
+
+	for(int bot = top; bot > 0; bot--){
+		combos += factorial(top)/(factorial(bot) * factorial(top-bot));
+	}
+	
+	int** combosArr = new int*[combos];
+
+	for (int i = 0; i < combos; i++){
+		combosArr[i] = 
+	}
+
+}
+
 int main(){
-	int specialNum = 100;
 
 	//test all combinations of num being prime fam
 	//move to next number in ones place
@@ -8,6 +27,7 @@ int main(){
 
 
 	
-	cout << specialNum << endl;
+	cout << getCombos(3) << endl;
 	return 0;
 }
+
