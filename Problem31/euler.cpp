82cd95294761274490d02e1cec7f4e36a1135b5c
dptheodore <dptheodore93@gmail.com>
2020-09-23 20:31:39 -0400

    more work in Problem 31

--- Problem31/euler.cpp
+++ Problem31/euler.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 const int COIN_CT = 8;
 const int denoms [COIN_CT] = {1,2,5,10,20,50,100,200};
-
+unsigned long long ways = 0;
 
 int waysOf(int num,int index){
-	int ways = 0;
-	if(num == 0){
+	if(num == 0 || num == 1){
 		return 1;
 	}
-	else if (num < denoms[0]){
+	else if (num < 0){
 		return 0;
 	}
 	else{
 		for(int i = index; i >= 0; i--){
 			ways += waysOf(num-denoms[i], i-1);
-			cout << i << endl;
+
+			cout << denoms[i] << " " << ways << " " << i  << endl;
 		}
 	}
 	return ways;
@@ -26,6 +26,6 @@ int waysOf(int num,int index){
 
 
 int main(){
-	cout << waysOf(200,COIN_CT-1) << endl;
+	cout << waysOf(4,COIN_CT-1) << endl;
 	return 0;
 }
